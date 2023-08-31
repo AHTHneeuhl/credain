6c4af1f55543aa6c697c8c6832b4aa892d4baeea
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-31 17:51:42 +0530

    feat: create modal and button components (#11)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,4 @@
 export { default as Typography } from "./typography";
 export { default as Input } from "./input";
+export { default as Modal } from "./modal";
+export { default as Button } from "./button";
