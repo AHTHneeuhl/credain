3297556a22e1d1db1ddf80a69c4d52fe6b12b9b9
Shubham Patrick <ahthneeuhl@gmail.com>
2023-08-31 14:24:46 +0530

    feat: add short and action icons

--- src/components/dashboard/TransactionTable.tsx
+++ src/components/dashboard/TransactionTable.tsx
@@ -1,3 +1,5 @@
+import { PiDotsThreeCircleVertical } from "react-icons/pi";
+import { BsArrowDownUp } from "react-icons/bs";
 import { transactions } from "config";
 import {
   Table,
@@ -11,17 +13,48 @@ import { Link } from "react-router-dom";
 import { Typography } from "components/common";
 
 const TransactionTable: React.FC = () => {
+  const handleSortStrings = () => {};
+  const handleSortNumbers = () => {};
+
   return (
     <Table className="my-12">
       <TableHead>
         <TableRow>
-          <TableHeader>Transaction Date</TableHeader>
-          <TableHeader>Invoice No.</TableHeader>
-          <TableHeader>Payer</TableHeader>
-          <TableHeader>Payee</TableHeader>
-          <TableHeader>Amount</TableHeader>
-          <TableHeader>USD Equivalent</TableHeader>
-          <TableHeader>Status</TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Transaction Date <BsArrowDownUp onClick={handleSortNumbers} />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Invoice No. <BsArrowDownUp onClick={handleSortStrings} />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Payer <BsArrowDownUp />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Payee <BsArrowDownUp />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Amount <BsArrowDownUp />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              USD Equivalent <BsArrowDownUp />
+            </div>
+          </TableHeader>
+          <TableHeader>
+            <div className="flex flex-row items-center gap-2">
+              Status <BsArrowDownUp />
+            </div>
+          </TableHeader>
           <TableHeader>Actions</TableHeader>
         </TableRow>
       </TableHead>
@@ -46,14 +79,19 @@ const TransactionTable: React.FC = () => {
               <TableCell>{payee}</TableCell>
               <TableCell>₹ {amount.toFixed(2)}</TableCell>
               <TableCell>$ {usdEquivalent.toFixed(2)}</TableCell>
-              <TableCell className="text-neutral-800 justify-start flex gap-1">
+              <TableCell className="text-neutral-800 justify-start flex gap-1 py-3">
                 {status.map((status, index) => (
                   <Typography key={index} className="bg-blue-100 rounded px-1">
                     {status}
                   </Typography>
                 ))}
               </TableCell>
-              <TableCell>action</TableCell>
+              <TableCell className="py-1">
+                <PiDotsThreeCircleVertical
+                  size={24}
+                  className="cursor-pointer text-center m-auto"
+                />
+              </TableCell>
             </TableRow>
           )
         )}
