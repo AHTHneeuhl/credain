b8a93adeb1d23e8782c9e0de07df8db9e2f80e24
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-31 10:58:51 +0530

    feat: create typography component and it's story (#5)

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,5 +1,23 @@
+import { Payer, TransactionDate } from "components/transaction";
+import { transactions } from "config";
+import { useParams } from "react-router-dom";
+
 const Transaction: React.FC = () => {
-  return null;
+  const { id } = useParams();
+
+  const transaction = transactions.find(
+    (transaction) => transaction.id === parseInt(id!)
+  );
+
+  return (
+    <>
+      <TransactionDate transactionDate={transaction?.transactionDate!} />
+      <div>Transaction Invoice - {transaction?.invoiceNumber}</div>
+      <Payer payer={transaction?.payer!} />
+      <div>Payee - {transaction?.payee}</div>
+      <div>Amount - {transaction?.amount.toFixed(2)}</div>
+    </>
+  );
 };
 
 export default Transaction;
