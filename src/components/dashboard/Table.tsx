9d6d01ab5e89fd4d74c27d5cddc719c95d3a5224
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-31 09:15:34 +0530

    feat: create transaction and table components (#4)

---
+++ src/components/dashboard/Table.tsx
@@ -0,0 +1,39 @@
+import { cn } from "helpers/utils";
+
+type TProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Table: React.FC<TProps> = ({ children }) => {
+  return <table className="w-full shadow-sm rounded-xl">{children}</table>;
+};
+
+export const TableHead: React.FC<TProps> = ({ children }) => {
+  return <thead className="bg-neutral-50 shadow-sm">{children}</thead>;
+};
+
+export const TableBody: React.FC<TProps> = ({ children }) => {
+  return <tbody>{children}</tbody>;
+};
+
+export const TableRow: React.FC<TProps> = ({ children }) => {
+  return <tr>{children}</tr>;
+};
+
+export const TableHeader: React.FC<TProps> = ({ children }) => {
+  return <th className="py-2">{children}</th>;
+};
+
+export const TableCell: React.FC<TProps> = ({ children, className }) => {
+  return (
+    <td
+      className={cn(
+        "text-center py-2 text-sm font-light text-neutral-500",
+        className
+      )}
+    >
+      {children}
+    </td>
+  );
+};
--- src/components/dashboard/TransactionTable.tsx
+++ src/components/dashboard/TransactionTable.tsx
@@ -1,5 +1,60 @@
+import { transactions } from "config";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./Table";
+import { Link } from "react-router-dom";
+
 const TransactionTable: React.FC = () => {
-  return <div>Transaction Table</div>;
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeader>Transaction Date</TableHeader>
+          <TableHeader>Invoice No.</TableHeader>
+          <TableHeader>Payer</TableHeader>
+          <TableHeader>Payee</TableHeader>
+          <TableHeader>Amount</TableHeader>
+          <TableHeader>USD Equivalent</TableHeader>
+          <TableHeader>Status</TableHeader>
+          <TableHeader>Actions</TableHeader>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {transactions.map(
+          ({
+            id,
+            transactionDate,
+            invoiceNumber,
+            payer,
+            payee,
+            amount,
+            usdEquivalent,
+            status,
+          }) => (
+            <TableRow key={id}>
+              <TableCell>{transactionDate}</TableCell>
+              <Link to={`/transaction/${id}`}>
+                <TableCell className="text-blue-700 underline cursor-pointer">
+                  {invoiceNumber}
+                </TableCell>
+              </Link>
+              <TableCell>{payer}</TableCell>
+              <TableCell>{payee}</TableCell>
+              <TableCell>₹ {amount.toFixed(2)}</TableCell>
+              <TableCell>$ {usdEquivalent.toFixed(2)}</TableCell>
+              <TableCell className="text-neutral-800">{status}</TableCell>
+              <TableCell>action</TableCell>
+            </TableRow>
+          )
+        )}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default TransactionTable;
